b5bc12e2a9120eca296fd72e8383ff41cabe9f7e
Arvid-GH <arvid.gunnerlinghulth@elev.ga.ntig.se>
2022-12-16 14:01:55 +0100

    skapa konto och logga in

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a list of users
+            var users = new List<User>();
+
+            while (true)
+            {
+                // Prompt the user to choose between creating a new account or logging in
+                Console.WriteLine("1. Create a new account");
+                Console.WriteLine("2. Log in");
+                Console.WriteLine("Enter your choice (1 or 2):");
+                var choice = Console.ReadLine();
+
+                if (choice == "1")
+                {
+                    // Prompt the user to enter their first name, surname, password, and social security number
+                    Console.WriteLine("Enter your first name:");
+                    var firstName = Console.ReadLine();
+                    Console.WriteLine("Enter your surname:");
+                    var surname = Console.ReadLine();
+                    Console.WriteLine("Enter your password:");
+                    var password = Console.ReadLine();
+                    Console.WriteLine("Enter your social security number:");
+                    var socialSecurityNumber = Console.ReadLine();
+
+                    // Create a new user with the entered information
+                    var user = new User { FirstName = firstName, Surname = surname, Password = password, SocialSecurityNumber = socialSecurityNumber };
+                    users.Add(user);
+
+                    Console.WriteLine("Account created successfully!");
+                }
+                else if (choice == "2")
+                {
+                    // Prompt the user to enter their first name, surname, password, and social security number
+                    Console.WriteLine("Enter your first name:");
+                    var firstName = Console.ReadLine();
+                    Console.WriteLine("Enter your surname:");
+                    var surname = Console.ReadLine();
+                    Console.WriteLine("Enter your password:");
+                    var password = Console.ReadLine();
+                    Console.WriteLine("Enter your social security number:");
+                    var socialSecurityNumber = Console.ReadLine();
+
+                    // Check if the first name, surname, password, and social security number are correct
+                    var user = users.FirstOrDefault(u => u.FirstName == firstName && u.Surname == surname && u.Password == password && u.SocialSecurityNumber == socialSecurityNumber);
+                    if (user != null)
+                    {
+                        Console.WriteLine("Login successful!");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid first name, surname, password, or social security number.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice.");
+                }
+            }
+        }
+    }
+
+    // Define the User class
+    class User
+    {
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string Password { get; set; }
+        public string SocialSecurityNumber { get; set; }
+    }
+}
--- P2/P2/Program.cs
+++ P2/P2/Program.cs
@@ -2,9 +2,50 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        class Login
         {
-            
+                
+            static void Main(string[] args)
+            {
+                // Create a list of users
+                var users = new[]
+                {
+                new User {FirstName = "John", Surname = "Doe", Password = "password1", SocialSecurityNumber = "123-45-6789"},
+                new User {FirstName = "Jane", Surname = "Doe", Password = "password2", SocialSecurityNumber = "987-65-4321"},
+                new User {FirstName = "Bob", Surname = "Smith", Password = "password3", SocialSecurityNumber = "456-78-9012"}
+            };
+
+                // Prompt the user to enter their first name, surname, password, and social security number
+                Console.WriteLine("Enter your first name:");
+                var firstName = Console.ReadLine();
+                Console.WriteLine("Enter your surname:");
+                var surname = Console.ReadLine();
+                Console.WriteLine("Enter your password:");
+                var password = Console.ReadLine();
+                Console.WriteLine("Enter your social security number:");
+                var socialSecurityNumber = Console.ReadLine();
+
+                // Check if the first name, surname, password, and social security number are correct
+                var user = users.FirstOrDefault(u => u.FirstName == firstName && u.Surname == surname && u.Password == password && u.SocialSecurityNumber == socialSecurityNumber);
+                if (user != null)
+                {
+                    Console.WriteLine("Login successful!");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid first name, surname, password, or social security number.");
+                }
+            }
+        }
+
+        // Define the User class
+        class User
+        {
+            public string FirstName { get; set; }
+            public string Surname { get; set; }
+            public string Password { get; set; }
+            public string SocialSecurityNumber { get; set; }
         }
     }
+    
 }
